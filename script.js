6a5f178c71002064bbc305ccf0f70006c8186a26
hsgwKei <63716217+hsgwKei@users.noreply.github.com>
2021-06-28 13:35:59 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -94,11 +94,11 @@ player.addEventListener('play', () => {
     .catch((e) => {
       consoloe.log(e)
     })*/
-    const pose = posenet.load().estimateSinglePose(player, imageScaleFactor, flipHorizontal, outputStride)
+    const pose = posenet.load().estimateSinglePose(player, imageScaleFactor, flipHorizontal, outputStride);
     p_text.textContent = "3";
     //p_text.textContent = pose['keypoints'][0]['position']['x']
     
-    //save_arr.push(createSaveData(detections[0], pose));
+    save_arr.push(createSaveData(detections[0], pose));
     //save_arr.push(createSaveData(detections[0]));
 
   }, 1000)
