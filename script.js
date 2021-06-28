336b67f6545c4c40700348451d2f324ad0e11e48
hsgwKei <63716217+hsgwKei@users.noreply.github.com>
2021-06-28 14:00:50 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -84,7 +84,7 @@ player.addEventListener('play', () => {
     const detections = await faceapi.detectAllFaces(player, new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks();
     p_text.textContent = "2";
     // posenet
-    posenet.load()
+    /*posenet.load()
     .then((net) => {
       return net.estimateSinglePose(player, imageScaleFactor, flipHorizontal, outputStride)
     })
@@ -93,10 +93,12 @@ player.addEventListener('play', () => {
     })
     .catch((e) => {
       consoloe.log(e)
-    })
+    })*/
+    pose = PoseFunc();
+
     //const pose = posenet.load().estimateSinglePose(player, imageScaleFactor, flipHorizontal, outputStride);
     //p_text.textContent = "3";
-    //p_text.textContent = pose['keypoints'][0]['position']['x']
+    p_text.textContent = pose['keypoints'][0]['position']['x']
     
     //save_arr.push(createSaveData(detections[0], pose));
     save_arr.push(createSaveData(detections[0]));
@@ -161,4 +163,17 @@ function createSaveData(detections) {
   }*/
 
   return arr;
+}
+
+function PoseFunc() {
+  posenet.load()
+  .then((net) => {
+    return net.estimateSinglePose(player, imageScaleFactor, flipHorizontal, outputStride)
+  })
+  .then((pose) => {
+    p_text.textContent = "no return"
+  })
+  .catch((e) => {
+    consoloe.log(e)
+  })
 }
